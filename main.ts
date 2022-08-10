1f4dae8059be8b6c940f67f7839e1e34f03f5e46
NovaKwan <110724796+NovaKwan@users.noreply.github.com>
2022-08-10 17:28:37 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+input.onSound(DetectedSound.Loud, function () {
+    music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Forever)
+})
+input.setSoundThreshold(SoundThreshold.Loud, 100)
 basic.forever(function () {
 	
 })
